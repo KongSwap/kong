083bc071713b0c5ad992bb11225e773fe94e59ea
kongzilla-ks <kongzilla@proton.me>
2024-10-27 10:26:41 +0100

    change local/staging/prod deploy scripts

--- scripts/local/upgrade_kong.sh
+++ scripts/local/upgrade_kong.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
 
-NETWORK=""
-IDENTITY="--identity kong"
-
-./switch_staging.sh
-
-dfx build ${NETWORK} ${IDENTITY} kong_backend
-dfx canister install ${NETWORK} ${IDENTITY} kong_backend --mode upgrade
-
-npm i kong_frontend
-
-dfx build ${NETWORK} ${IDENTITY} kong_frontend
-dfx canister install ${NETWORK} ${IDENTITY} kong_frontend --mode upgrade
+./upgrade_kong_backend.sh
+./upgrade_kong_frontend.sh
