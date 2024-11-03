776ed1cc5b58ae6a773a68724e21aa322cc7160e
kongzilla-ks <kongzilla@proton.me>
2024-11-03 20:44:47 +0100

    added backup for archive maps

--- src/kong_backend/src/controllers/requests.rs
+++ src/kong_backend/src/controllers/requests.rs
@@ -4,7 +4,7 @@ use std::collections::BTreeMap;
 use crate::ic::guards::caller_is_kingkong;
 use crate::requests::request_reply::RequestReply;
 use crate::requests::request_reply_impl::to_request_reply;
-use crate::stable_memory::REQUEST_MAP;
+use crate::stable_memory::{REQUEST_ARCHIVE_MAP, REQUEST_MAP};
 use crate::stable_request::request_map;
 use crate::stable_request::stable_request::StableRequestId;
 
@@ -13,6 +13,26 @@ const MAX_REQUESTS: usize = 1_000;
 #[query(hidden = true, guard = "caller_is_kingkong")]
 fn backup_requests(request_id: Option<u64>, num_requests: Option<u16>) -> Result<String, String> {
     REQUEST_MAP.with(|m| {
+        let map = m.borrow();
+        let requests: BTreeMap<_, _> = match request_id {
+            Some(request_id) => {
+                let num_requests = num_requests.map_or(1, |n| n as usize);
+                let start_key = StableRequestId(request_id);
+                map.range(start_key..).take(num_requests).collect()
+            }
+            None => {
+                let num_requests = num_requests.map_or(MAX_REQUESTS, |n| n as usize);
+                map.iter().take(num_requests).collect()
+            }
+        };
+
+        serde_json::to_string(&requests).map_err(|e| format!("Failed to serialize requests: {}", e))
+    })
+}
+
+#[query(hidden = true, guard = "caller_is_kingkong")]
+fn backup_archive_requests(request_id: Option<u64>, num_requests: Option<u16>) -> Result<String, String> {
+    REQUEST_ARCHIVE_MAP.with(|m| {
         let map = m.borrow();
         let requests: BTreeMap<_, _> = match request_id {
             Some(request_id) => {
