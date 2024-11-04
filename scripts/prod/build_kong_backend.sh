e6b4e07838e5ba60a3895cf9bf9ba43c71e06ab6
kongzilla-ks <kongzilla@proton.me>
2024-11-04 19:39:03 +0100

    prod deploy scripts

--- scripts/prod/build_kong_backend.sh
+++ scripts/prod/build_kong_backend.sh
@@ -5,3 +5,8 @@ NETWORK="--network ic"
 ./switch_prod.sh
 
 dfx build ${NETWORK} ${IDENTITY} kong_backend
+
+cd ../../target/wasm32-unknown-unknown/release
+ic-wasm kong_backend.wasm -o kong_backend_opt.wasm optimize O3
+gzip -c kong_backend_opt.wasm > kong_backend.wasm.gz
+cd ../../../scripts/prod
