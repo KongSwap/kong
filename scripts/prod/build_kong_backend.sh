fcfd268cc015d477903ad1268e7f2e615c1a12fc
kongzilla-ks <kongzilla@proton.me>
2024-11-04 22:46:16 +0100

    bugs: removed ic-wasm opt, ckusdt same pool

--- scripts/prod/build_kong_backend.sh
+++ scripts/prod/build_kong_backend.sh
@@ -7,6 +7,5 @@ NETWORK="--network ic"
 dfx build ${NETWORK} ${IDENTITY} kong_backend
 
 cd ../../target/wasm32-unknown-unknown/release
-ic-wasm kong_backend.wasm -o kong_backend_opt.wasm optimize O3
-gzip -c kong_backend_opt.wasm > kong_backend.wasm.gz
+gzip kong_backend_opt.wasm
 cd ../../../scripts/prod
