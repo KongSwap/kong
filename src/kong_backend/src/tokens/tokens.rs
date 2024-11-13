672b6e407fcbb10124d8cd5cdd4cc0aa876f7db2
kongzilla-ks <kongzilla@proton.me>
2024-11-13 20:12:06 +0100

    removed pool sorting

--- src/kong_backend/src/tokens/tokens.rs
+++ src/kong_backend/src/tokens/tokens.rs
@@ -14,9 +14,10 @@ use crate::stable_token::token_map;
 /// None returns tokens only listed on Kong
 #[query(guard = "not_in_maintenance_mode")]
 fn tokens(symbol: Option<String>) -> Result<Vec<TokensReply>, String> {
-    Ok(match symbol.as_deref() {
+    let tokens = match symbol.as_deref() {
         Some("all") => token_map::get().iter().map(to_token_reply).collect(),
         Some(symbol) => token_map::get_by_token_wildcard(symbol).iter().map(to_token_reply).collect(),
         None => token_map::get_on_kong().iter().map(to_token_reply).collect(),
-    })
+    };
+    Ok(tokens)
 }
