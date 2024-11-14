5353c273a984e3bb9ce9d2f6e7e9951ee8dbbda6
Jon Lee <jon.honming.lee@gmail.com>
2024-11-14 16:07:39 +0100

    moved the update functions from kong_data to kong_backend

--- src/kong_backend/src/controllers/tokens.rs
+++ src/kong_backend/src/controllers/tokens.rs
@@ -1,28 +1,47 @@
-use ic_cdk::query;
+use ic_cdk::{query, update};
 use std::collections::BTreeMap;
 
 use crate::ic::guards::caller_is_kingkong;
 use crate::stable_memory::TOKEN_MAP;
-use crate::stable_token::stable_token::StableTokenId;
+use crate::stable_token::stable_token::{StableToken, StableTokenId};
 
 const MAX_TOKENS: usize = 1_000;
 
+/// serializes TOKEN_MAP for backup
 #[query(hidden = true, guard = "caller_is_kingkong")]
 fn backup_tokens(token_id: Option<u32>, num_tokens: Option<u16>) -> Result<String, String> {
     TOKEN_MAP.with(|m| {
         let map = m.borrow();
         let tokens: BTreeMap<_, _> = match token_id {
             Some(token_id) => {
+                let start_id = StableTokenId(token_id);
                 let num_tokens = num_tokens.map_or(1, |n| n as usize);
-                let start_key = StableTokenId(token_id);
-                map.range(start_key..).take(num_tokens).collect()
+                map.range(start_id..).take(num_tokens).collect()
             }
             None => {
                 let num_tokens = num_tokens.map_or(MAX_TOKENS, |n| n as usize);
                 map.iter().take(num_tokens).collect()
             }
         };
-
         serde_json::to_string(&tokens).map_err(|e| format!("Failed to serialize tokens: {}", e))
     })
+}
+
+/// deserialize TOKEN_MAP and update stable memory
+#[update(hidden = true, guard = "caller_is_kingkong")]
+fn update_tokens(stable_tokens: String) -> Result<String, String> {
+    let tokens: BTreeMap<StableTokenId, StableToken> = match serde_json::from_str(&stable_tokens) {
+        Ok(tokens) => tokens,
+        Err(e) => return Err(format!("Invalid tokens: {}", e)),
+    };
+
+    TOKEN_MAP.with(|user_map| {
+        let mut map = user_map.borrow_mut();
+        map.clear_new();
+        for (k, v) in tokens {
+            map.insert(k, v);
+        }
+    });
+
+    Ok("Tokens updated".to_string())
 }
