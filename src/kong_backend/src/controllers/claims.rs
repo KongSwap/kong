10c7982565d00a5f614f0a9e36d5afdda631a856
Jon Lee <jon.honming.lee@gmail.com>
2024-11-14 16:27:14 +0100

    backup+update for all stable memory

--- src/kong_backend/src/controllers/claims.rs
+++ src/kong_backend/src/controllers/claims.rs
@@ -7,22 +7,22 @@ use crate::stable_memory::CLAIM_MAP;
 
 const MAX_CLAIMS: usize = 1_000;
 
+/// serialize CLAIM_MAP for backup
 #[query(hidden = true, guard = "caller_is_kingkong")]
 fn backup_claims(claim_id: Option<u64>, num_claims: Option<u16>) -> Result<String, String> {
     CLAIM_MAP.with(|m| {
         let map = m.borrow();
         let claims: BTreeMap<_, _> = match claim_id {
             Some(claim_id) => {
+                let start_id = StableClaimId(claim_id);                
                 let num_claims = num_claims.map_or(1, |n| n as usize);
-                let start_key = StableClaimId(claim_id);
-                map.range(start_key..).take(num_claims).collect()
+                map.range(start_id..).take(num_claims).collect()
             }
             None => {
                 let num_claims = num_claims.map_or(MAX_CLAIMS, |n| n as usize);
                 map.iter().take(num_claims).collect()
             }
         };
-
         serde_json::to_string(&claims).map_err(|e| format!("Failed to serialize claims: {}", e))
     })
 }
--- src/kong_backend/src/controllers/messages.rs
+++ src/kong_backend/src/controllers/messages.rs
@@ -11,22 +11,22 @@ use crate::stable_message::stable_message::{StableMessage, StableMessageId};
 
 const MAX_MESSAGE: usize = 1_000;
 
+/// serialize MESSAGE_MAP for backup
 #[query(hidden = true, guard = "caller_is_kingkong")]
 fn backup_messages(message_id: Option<u64>, num_messages: Option<u16>) -> Result<String, String> {
     MESSAGE_MAP.with(|m| {
         let map = m.borrow();
         let messages: BTreeMap<_, _> = match message_id {
             Some(message_id) => {
+                let start_id = StableMessageId(message_id);
                 let num_messages = num_messages.map_or(1, |n| n as usize);
-                let start_key = StableMessageId(message_id);
-                map.range(start_key..).take(num_messages).collect()
+                map.range(start_id..).take(num_messages).collect()
             }
             None => {
                 let num_messages = num_messages.map_or(MAX_MESSAGE, |n| n as usize);
                 map.iter().take(num_messages).collect()
             }
         };
-
         serde_json::to_string(&messages).map_err(|e| format!("Failed to serialize messages: {}", e))
     })
 }
