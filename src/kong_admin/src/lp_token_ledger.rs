16fe9c599776ecb5cb61d7ab9f1d3284d84daa2c
kongzilla-ks <kongzilla@proton.me>
2024-11-15 20:41:06 +0100

    ../../../scripts/reinstall_kong_data.sh

--- src/kong_admin/src/lp_token_ledger.rs
+++ src/kong_admin/src/lp_token_ledger.rs
@@ -1,7 +1,9 @@
 use kong_lib::stable_lp_token_ledger::stable_lp_token_ledger::{StableLPTokenLedger, StableLPTokenLedgerId};
 use num_traits::ToPrimitive;
+use regex::Regex;
 use serde_json::json;
 use std::collections::BTreeMap;
+use std::fs;
 use std::fs::File;
 use std::io::{BufReader, Read};
 use std::path::Path;
@@ -21,47 +23,94 @@ pub fn serialize_lp_token_ledger(lp_token_ledger: &StableLPTokenLedger) -> serde
 }
 
 pub async fn dump_lp_token_ledger(db_client: &Client, tokens_map: &BTreeMap<u32, u8>) -> Result<(), Box<dyn std::error::Error>> {
-    let file = File::open("./backups/lp_token_ledger.json")?;
-    let reader = BufReader::new(file);
-    let lp_token_ledger_map: BTreeMap<StableLPTokenLedgerId, StableLPTokenLedger> = serde_json::from_reader(reader)?;
+    let dir_path = "./backups";
+    let re_pattern = Regex::new(r"lp_token_ledger.*.json").unwrap();
+    let mut files = fs::read_dir(dir_path)?
+        .filter_map(|entry| entry.ok())
+        .filter_map(|entry| {
+            if re_pattern.is_match(entry.file_name().to_str().unwrap()) {
+                Some(entry)
+            } else {
+                None
+            }
+        })
+        .map(|entry| {
+            // sort by the number in the filename
+            let file = entry.path();
+            let filename = Path::new(&file).file_name().unwrap().to_str().unwrap();
+            let number_str = filename.split('.').nth(1).unwrap();
+            let number = number_str.parse::<u32>().unwrap();
+            (number, file)
+        })
+        .collect::<Vec<_>>();
+    files.sort_by(|a, b| a.0.cmp(&b.0));
 
-    for (k, v) in lp_token_ledger_map.iter() {
-        let lp_token_id = v.lp_token_id as i64;
-        let user_id = v.user_id as i32;
-        let token_id = v.token_id as i32;
-        let decimals = tokens_map.get(&v.token_id).ok_or(format!("token_id={} not found", v.token_id))?;
-        let amount = round_f64(v.amount.0.to_f64().unwrap() / 10_u64.pow(*decimals as u32) as f64, *decimals);
-        let ts = v.ts as f64 / 1_000_000_000.0;
-        let raw_json = serialize_lp_token_ledger(v);
+    for file in files {
+        let file = File::open(file.1)?;
+        let reader = BufReader::new(file);
+        let lp_token_ledger_map: BTreeMap<StableLPTokenLedgerId, StableLPTokenLedger> = serde_json::from_reader(reader)?;
 
-        db_client
-            .execute(
-                "INSERT INTO lp_token_ledger 
-                    (lp_token_id, user_id, token_id, amount, ts, raw_json)
-                    VALUES ($1, $2, $3, $4, to_timestamp($5), $6)
-                    ON CONFLICT (lp_token_id) DO UPDATE SET
-                        user_id = $2,
-                        token_id = $3,
-                        amount = $4,
-                        ts = to_timestamp($5),
-                        raw_json = $6",
-                &[&lp_token_id, &user_id, &token_id, &amount, &ts, &raw_json],
-            )
-            .await?;
-        println!("lp_token_id={} saved", k.0);
+        for (k, v) in lp_token_ledger_map.iter() {
+            let lp_token_id = v.lp_token_id as i64;
+            let user_id = v.user_id as i32;
+            let token_id = v.token_id as i32;
+            let decimals = tokens_map.get(&v.token_id).ok_or(format!("token_id={} not found", v.token_id))?;
+            let amount = round_f64(v.amount.0.to_f64().unwrap() / 10_u64.pow(*decimals as u32) as f64, *decimals);
+            let ts = v.ts as f64 / 1_000_000_000.0;
+            let raw_json = serialize_lp_token_ledger(v);
+
+            db_client
+                .execute(
+                    "INSERT INTO lp_token_ledger 
+                        (lp_token_id, user_id, token_id, amount, ts, raw_json)
+                        VALUES ($1, $2, $3, $4, to_timestamp($5), $6)
+                        ON CONFLICT (lp_token_id) DO UPDATE SET
+                            user_id = $2,
+                            token_id = $3,
+                            amount = $4,
+                            ts = to_timestamp($5),
+                            raw_json = $6",
+                    &[&lp_token_id, &user_id, &token_id, &amount, &ts, &raw_json],
+                )
+                .await?;
+            println!("lp_token_id={} saved", k.0);
+        }
     }
 
     Ok(())
 }
 
 pub async fn update_lp_token_ledger<T: KongUpdate>(kong_update: &T) -> Result<(), Box<dyn std::error::Error>> {
-    let path = Path::new("./backups/lp_token_ledger.json");
-    let file = File::open(path)?;
-    println!("processing: {:?}", path.file_name().unwrap());
-    let mut reader = BufReader::new(file);
-    let mut contents = String::new();
-    reader.read_to_string(&mut contents)?;
-    kong_update.update_lp_token_ledger(&contents).await?;
+    let dir_path = "./backups";
+    let re_pattern = Regex::new(r"lp_token_ledger.*.json").unwrap();
+    let mut files = fs::read_dir(dir_path)?
+        .filter_map(|entry| entry.ok())
+        .filter_map(|entry| {
+            if re_pattern.is_match(entry.file_name().to_str().unwrap()) {
+                Some(entry)
+            } else {
+                None
+            }
+        })
+        .map(|entry| {
+            // sort by the number in the filename
+            let file = entry.path();
+            let filename = Path::new(&file).file_name().unwrap().to_str().unwrap();
+            let number_str = filename.split('.').nth(1).unwrap();
+            let number = number_str.parse::<u32>().unwrap();
+            (number, file)
+        })
+        .collect::<Vec<_>>();
+    files.sort_by(|a, b| a.0.cmp(&b.0));
+
+    for file in files {
+        println!("processing: {:?}", file.1.file_name().unwrap());
+        let file = File::open(file.1)?;
+        let mut reader = BufReader::new(file);
+        let mut contents = String::new();
+        reader.read_to_string(&mut contents)?;
+        kong_update.update_lp_token_ledger(&contents).await?;
+    }
 
     Ok(())
 }
