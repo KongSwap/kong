6c1fc799767e6ab52b09cc0ec3664dd2f7f57036
Caner <54007585+CanerKocak@users.noreply.github.com>
2024-11-16 14:21:28 +0100

    remove unnneeded

--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  onMount(() => {
-    const navbar = document.querySelector('.navbar'); // Adjust selector
-    if (navbar) {
-      const height = navbar.getBoundingClientRect().height;
-      document.documentElement.style.setProperty('--navbar-height', `${height}px`);
-    }
-  });
-</script> 
