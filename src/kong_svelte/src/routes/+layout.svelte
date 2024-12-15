9e477db15646f2995cb67c818222faaca9412b34
usern3 <nolan@syslogica.io>
2024-12-15 11:54:25 -0600

    details panel update

--- src/kong_svelte/src/routes/+layout.svelte
+++ src/kong_svelte/src/routes/+layout.svelte
@@ -8,16 +8,13 @@
   import { appLoader } from "$lib/services/appLoader";
   import PageWrapper from "$lib/components/layout/PageWrapper.svelte";
   import { updateWorkerService } from "$lib/services/updateWorkerService";
-  import { auth } from "$lib/services/auth";
   import AddToHomeScreen from "$lib/components/common/AddToHomeScreen.svelte";
 
-  let pageTitle = $state(
-    process.env.DFX_NETWORK === "ic" ? "KongSwap" : "KongSwap [DEV]",
-  );
   let { children } = $props();
+  let pageTitle = $state(process.env.DFX_NETWORK === "ic" ? "KongSwap" : "KongSwap [DEV]");
   let initializationPromise: Promise<void> | null = null;
 
-  async function init() {
+  function init() {
     if (initializationPromise) {
       return initializationPromise;
     }
@@ -25,7 +22,6 @@
     initializationPromise = (async () => {
       try {
         await appLoader.initialize();
-        await auth.initialize();
       } catch (error) {
         console.error("Initialization error:", error);
         initializationPromise = null;
