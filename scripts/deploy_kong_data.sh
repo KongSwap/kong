c80a40c97f8a6a9693b186c6701c156338e8a170
kongzilla-ks <kongzilla@proton.me>
2024-12-17 07:41:55 +0100

    build for kong_faucet

--- scripts/deploy_kong_data.sh
+++ scripts/deploy_kong_data.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
-original_dir=$(pwd)
-root_dir="${original_dir}"/..
+NETWORK="--network $1"
+IDENTITY="--identity kong"
+KONG_DATA=kong_data
 
 if [ "$1" == "ic" ]; then
     bash create_canister_id.sh ic
-    KONG_BUILDENV="ic" dfx build kong_data --network ic
+    KONG_BUILDENV="ic" dfx build ${KONG_DATA} ${NETWORK}
     bash gzip_kong_data.sh ic
 elif [ "$1" == "staging" ]; then
     bash create_canister_id.sh staging
-    KONG_BUILDENV="staging" dfx deploy kong_data --network staging
+    KONG_BUILDENV="staging" dfx deploy ${KONG_DATA} ${NETWORK} ${IDENTITY}
 elif [ "$1" == "local" ]; then
+    original_dir=$(pwd)
+    root_dir="${original_dir}"/..
     if CANISTER_ID=$(jq -r ".[\"kong_data\"][\"local\"]" "${root_dir}"/canister_ids.all.json); then
         [ "${CANISTER_ID}" != "null" ] && {
-            echo "Deploying kong_data with ID: ${CANISTER_ID}"
-            KONG_BUILDENV="local" dfx deploy kong_data --network local --specified-id "${CANISTER_ID}" || true
+            SPECIFIED_ID="--specified-id ${CANISTER_ID}"
+            KONG_BUILDENV="local" dfx deploy ${KONG_DATA} ${NETWORK} ${IDENTITY} ${SPECIFIED_ID} || true
         }
     fi
 fi
