24b2ab1cbc7c3684566467777a7b2cc673b7d489
usern3 <nolan@syslogica.io>
2025-01-09 09:29:06 -0600

    padding

--- src/kong_svelte/src/lib/components/common/Modal.svelte
+++ src/kong_svelte/src/lib/components/common/Modal.svelte
@@ -208,7 +208,7 @@
             <div class="drag-handle touch-pan-x"></div>
 
             <header
-              class="flex justify-between items-center flex-shrink-0 px-3 py-2"
+              class="flex justify-between items-center flex-shrink-0 px-3 pb-2 pt-3"
             >
               <slot name="title">
                 {#if typeof title === "string"}
