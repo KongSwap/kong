66d0be304cd9868d3e793dca599b8fd01846c83f
usern3 <nolan@syslogica.io>
2025-02-11 21:36:25 -0600

    fix deploy_kong script ocassional failures

--- scripts/deploy_kong_svelte.sh
+++ scripts/deploy_kong_svelte.sh
@@ -4,6 +4,9 @@ original_dir=$(pwd)
 root_dir="${original_dir}"/..
 static_dir="${root_dir}/src/kong_svelte/static/.well-known"
 
+# Copy canister IDs file
+cp "${root_dir}"/canister_ids.all.json "${root_dir}"/canister_ids.json
+
 # Create .well-known directory if it doesn't exist
 mkdir -p "${static_dir}"
 
@@ -77,19 +80,17 @@ npm i
 
 if [ $1 == "ic" ]; then
     bash create_canister_id.sh ic
-    CANISTER_ID=$(jq -r ".[\"kong_svelte\"][\"ic\"]" "${root_dir}"/canister_ids.all.json)
+    CANISTER_ID=$(jq -r ".[\"kong_svelte\"][\"ic\"]" "${root_dir}"/canister_ids.json)
     create_static_files "ic" "${CANISTER_ID}"
     dfx build kong_svelte --network ic
 elif [ $1 == "staging" ]; then
     bash create_canister_id.sh staging
-    CANISTER_ID=$(jq -r ".[\"kong_svelte\"][\"staging\"]" "${root_dir}"/canister_ids.all.json)
+    CANISTER_ID=$(jq -r ".[\"kong_svelte\"][\"staging\"]" "${root_dir}"/canister_ids.json)
     create_static_files "staging" "${CANISTER_ID}"
     dfx deploy kong_svelte --network staging
 elif [ $1 == "local" ]; then
-    if CANISTER_ID=$(jq -r ".[\"kong_svelte\"][\"local\"]" "${root_dir}"/canister_ids.all.json); then
-        [ "${CANISTER_ID}" != "null" ] && {
-            create_static_files "local" "${CANISTER_ID}"
-            dfx deploy kong_svelte --network local --specified-id "${CANISTER_ID}" || true
-        }
-    fi
+    CANISTER_ID=$(jq -r ".[\"kong_svelte\"][\"local\"]" "${root_dir}"/canister_ids.json)
+    echo "CANISTER_ID: ${CANISTER_ID}"
+    create_static_files "local" "${CANISTER_ID}"
+    dfx deploy kong_svelte --specified-id "${CANISTER_ID}"
 fi
