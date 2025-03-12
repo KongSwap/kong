897084fbc50a5742b68bff4c7e376c59411aa6b0
kongzilla-ks <kongzilla@proton.me>
2025-03-12 19:49:55 +0100

    added canister_id to claims

--- src/kong_backend/src/claims/claims_reply.rs
+++ src/kong_backend/src/claims/claims_reply.rs
@@ -1,14 +1,13 @@
 use candid::{CandidType, Nat};
 use serde::{Deserialize, Serialize};
 
-/// Data structure for the reply of the `claim` function.
-/// Used in StableRequest
 #[derive(CandidType, Debug, Clone, Serialize, Deserialize)]
 pub struct ClaimsReply {
     pub claim_id: u64,
     pub status: String,
     pub chain: String,
     pub symbol: String,
+    pub canister_id: Option<String>,
     pub amount: Nat,
     pub fee: Nat,
     pub to_address: String,
