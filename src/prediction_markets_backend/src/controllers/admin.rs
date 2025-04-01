f480842cb1893f9ac8470c26190f020a19eb4e7b
usern3 <59351042+usern3@users.noreply.github.com>
2025-03-31 21:10:16 -0500

    fix: update timezone handling to use the user's local timezone and adjust token state management, add aaron as prediction admin

--- src/prediction_markets_backend/src/controllers/admin.rs
+++ src/prediction_markets_backend/src/controllers/admin.rs
@@ -3,7 +3,7 @@ use std::cell::RefCell;
 use std::collections::HashSet;
 use ic_cdk::query;
 
-const DEFAULT_ADMIN_PRINCIPALS: [&str; 6] = [
+const DEFAULT_ADMIN_PRINCIPALS: [&str; 7] = [
     "4jxje-hbmra-4otqc-6hor3-cpwlh-sqymk-6h4ef-42sqn-o3ip5-s3mxk-uae",
     "6rjil-isfbu-gsmpe-ffvcl-v3ifl-xqgkr-en2ir-pbr54-cetku-syp4i-bae",
     "hkxzv-wmenl-q4d3b-j3o5s-yucpn-g5itu-b3zmq-hxggl-s3atg-vryjf-dqe",
@@ -11,6 +11,8 @@ const DEFAULT_ADMIN_PRINCIPALS: [&str; 6] = [
     "7ohni-sbpse-y327l-syhzk-jn6n4-hw277-erei5-xhkjr-lbh6b-rjqei-sqe",
     "6ydau-gqejl-yqbq7-tm2i5-wscbd-lsaxy-oaetm-dxddd-s5rtd-yrpq2-eae",
     "bc4tr-kdoww-zstxb-plqge-bo6ho-abuc2-mft22-6tdpb-5ofll-yknor-sae",
+    // Aaron
+    "m6wjp-mi46v-ekfrp-lu3wo-ero7s-e2y57-yu4kv-235o5-bnmti-qjpgk-aqe"
 ];
 
 thread_local! {
