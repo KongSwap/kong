89663a9914276c4ac01d1b0bf69663e518ee4f5a
usern3 <59351042+usern3@users.noreply.github.com>
2025-04-09 16:00:21 -0500

    ui enhancements

--- src/kong_svelte/src/lib/components/swap/swap_ui/SwitchTokensButton.svelte
+++ src/kong_svelte/src/lib/components/swap/swap_ui/SwitchTokensButton.svelte
@@ -99,7 +99,7 @@
     style={iconStyle}
   >
     <svg
-      class="w-6 h-6 opacity-90"
+      class="w-5 h-5 sm:w-6 sm:h-6 opacity-90"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
