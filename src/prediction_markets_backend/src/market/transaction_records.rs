2c219d7e9fdba14f262c588c1d14925e4c049e97
prim8bits <fangsey@protonmail.com>
2025-04-29 21:05:44 +0200

    staged files

---
+++ src/prediction_markets_backend/src/market/transaction_records.rs
@@ -0,0 +1,75 @@
+use candid::{CandidType, Principal};
+use serde::{Deserialize, Serialize};
+
+use crate::market::estimate_return_types::BetPayoutRecord;
+
+use crate::nat::*;
+
+
+
+/// Collection of payout records for a market
+#[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
+pub struct MarketPayoutRecords {
+    pub market_id: MarketId,
+    pub payouts: Vec<BetPayoutRecord>,
+    pub total_payout: StorableNat,
+    pub payout_count: usize,
+    pub uses_time_weighting: bool,
+}
+
+impl MarketPayoutRecords {
+    pub fn new(market_id: MarketId, uses_time_weighting: bool) -> Self {
+        Self {
+            market_id,
+            payouts: Vec::new(),
+            total_payout: StorableNat::from(0u64),
+            payout_count: 0,
+            uses_time_weighting,
+        }
+    }
+
+    pub fn add_payout(&mut self, payout: BetPayoutRecord) {
+        self.total_payout = self.total_payout.clone() + payout.payout_amount.clone();
+        self.payouts.push(payout);
+        self.payout_count += 1;
+    }
+}
+
+/// Summary of payouts for a user across all markets
+#[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
+pub struct UserPayoutSummary {
+    pub user: Principal,
+    pub total_payouts: StorableNat,
+    pub payout_count: usize,
+    pub markets_participated: Vec<MarketId>,
+    pub time_weighted_payouts: StorableNat,
+    pub standard_payouts: StorableNat,
+}
+
+impl UserPayoutSummary {
+    pub fn new(user: Principal) -> Self {
+        Self {
+            user,
+            total_payouts: StorableNat::from(0u64),
+            payout_count: 0,
+            markets_participated: Vec::new(),
+            time_weighted_payouts: StorableNat::from(0u64),
+            standard_payouts: StorableNat::from(0u64),
+        }
+    }
+
+    pub fn add_payout(&mut self, payout: &BetPayoutRecord) {
+        self.total_payouts = self.total_payouts.clone() + payout.payout_amount.clone();
+        self.payout_count += 1;
+        
+        if !self.markets_participated.contains(&payout.market_id) {
+            self.markets_participated.push(payout.market_id.clone());
+        }
+        
+        if payout.was_time_weighted {
+            self.time_weighted_payouts = self.time_weighted_payouts.clone() + payout.payout_amount.clone();
+        } else {
+            self.standard_payouts = self.standard_payouts.clone() + payout.payout_amount.clone();
+        }
+    }
+}
