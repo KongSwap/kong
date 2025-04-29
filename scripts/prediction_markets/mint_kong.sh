dbfa100bd629ee7944a5a3ae65bc703e6feab365
usern3 <59351042+usern3@users.noreply.github.com>
2025-04-29 08:04:54 -0500

    fix local deploy

--- scripts/prediction_markets/mint_kong.sh
+++ scripts/prediction_markets/mint_kong.sh
@@ -18,7 +18,7 @@ principals=(
 AMOUNT=5_000_000_000_000  # 50000 * 10^8
 
 # Get canister ID
-TOKEN_SYMBOL="ksKONG"
+TOKEN_SYMBOL="KONG"
 TOKEN_LEDGER=$(echo ${TOKEN_SYMBOL}_ledger | tr '[:upper:]' '[:lower:]')
 CANISTER_ID=$(dfx canister id ${TOKEN_LEDGER})
 
--- scripts/prediction_markets/setup.sh
+++ scripts/prediction_markets/setup.sh
@@ -9,7 +9,7 @@ echo "Done."
 
 dfx identity use minter
 
-echo "Deploying kskong_ledger..."
+echo "Deploying kong_ledger..."
 ./deploy_kong_ledger.sh local
 
 dfx identity use default
