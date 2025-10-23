a2e74c83c5e1ab07343e45fd667e12eb675078d6
Ilya Skapenko <ilyaskapenko@gmail.com>
2025-10-23 09:46:16 +0300

    Add pagination to tokens

--- src/kong_backend/src/tokens/tokens.rs
+++ src/kong_backend/src/tokens/tokens.rs
@@ -5,16 +5,15 @@ use super::tokens_reply_helpers::to_token_reply;
 
 use crate::ic::guards::not_in_maintenance_mode;
 use crate::stable_token::token_map;
+use crate::stable_token::tokens_args::TokensArgs;
+
 
 #[query(guard = "not_in_maintenance_mode")]
-fn tokens(symbol: Option<String>) -> Result<Vec<TokensReply>, String> {
-    let tokens = match symbol.as_deref() {
-        Some(symbol) => token_map::get_by_token_wildcard(symbol),
-        None => token_map::get(),
-    }
-    .iter()
-    .map(to_token_reply)
-    .collect();
+fn tokens(tokens_args: Option<TokensArgs>) -> Result<Vec<TokensReply>, String> {
+    let tokens = match tokens_args {
+        None => token_map::get_and_map(to_token_reply),
+        Some(v) => token_map::get_and_map_tokens_args(v, to_token_reply),
+    };
 
     Ok(tokens)
 }
