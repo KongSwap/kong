a4c1f5f4efce673599b9d020fc032c4063855ad2
Ilya Skapenko <ilyaskapenko@gmail.com>
2025-10-23 10:43:57 +0300

    Introduce tokens_v2

--- src/kong_backend/src/tokens/tokens.rs
+++ src/kong_backend/src/tokens/tokens.rs
@@ -9,7 +9,20 @@ use crate::stable_token::tokens_args::TokensArgs;
 
 
 #[query(guard = "not_in_maintenance_mode")]
-fn tokens(tokens_args: Option<TokensArgs>) -> Result<Vec<TokensReply>, String> {
+fn tokens(symbol: Option<String>) -> Result<Vec<TokensReply>, String> {
+    let tokens = match symbol.as_deref() {
+        Some(symbol) => token_map::get_by_token_wildcard(symbol),
+        None => token_map::get(),
+    }
+    .iter()
+    .map(to_token_reply)
+    .collect();
+
+    Ok(tokens)
+}
+
+#[query(guard = "not_in_maintenance_mode")]
+fn tokens_v2(tokens_args: Option<TokensArgs>) -> Result<Vec<TokensReply>, String> {
     let tokens = match tokens_args {
         None => token_map::get_and_map(to_token_reply),
         Some(v) => token_map::get_and_map_tokens_args(v, to_token_reply),
--- src/kong_data/src/stable_token/token_map.rs
+++ src/kong_data/src/stable_token/token_map.rs
@@ -2,6 +2,7 @@ use wildmatch::WildMatch;
 
 use crate::stable_memory::TOKEN_MAP;
 use crate::stable_token::stable_token::{StableToken, StableTokenId};
+use crate::stable_token::tokens_args::TokensArgs;
 
 use super::token::Token;
 
@@ -25,12 +26,56 @@ pub fn get_by_token_wildcard(token: &str) -> Vec<StableToken> {
     })
 }
 
+pub fn get_and_map_tokens_args<U, F>(tokens_args: TokensArgs, mapper: F) -> Vec<U>
+where
+    F: Fn(&StableToken) -> U,
+{
+    let n_take = tokens_args.n_take.unwrap_or(u32::MAX) as usize;
+    let n_skip = tokens_args.n_skip.unwrap_or(0) as usize;
+
+    TOKEN_MAP.with(|m| {
+        let m = m.borrow();
+        let mut it: Box<dyn Iterator<Item = StableToken>> = Box::new(m.iter().map(|(_, v)| v));
+
+        if tokens_args.enabled_only {
+            it = Box::new(it.filter_map(|v| if !v.is_removed() { Some(v) } else { None }));
+        }
+
+        if let Some(wildcard) = tokens_args.wildcard {
+            let wildmatch = WildMatch::new(&format!("*{}*", wildcard));
+            it = Box::new(it.filter_map(move |v| {
+                if wildmatch.matches(&v.symbol()) || wildmatch.matches(&v.address()) {
+                    Some(v)
+                } else {
+                    None
+                }
+            }));
+        }
+
+        it.skip(n_skip).take(n_take).map(|v| mapper(&v)).collect()
+    })
+}
+
 /// return all tokens
 pub fn get() -> Vec<StableToken> {
     TOKEN_MAP.with(|m| {
         m.borrow()
             .iter()
             .filter_map(|(_, v)| if !v.is_removed() { Some(v) } else { None })
+            .collect()
+    })
+}
+
+// return all mapped tokens
+pub fn get_and_map<U, F>(mapper: F) -> Vec<U>
+where
+    F: Fn(&StableToken) -> U,
+{
+    TOKEN_MAP.with(|m| {
+        m.borrow()
+            .iter()
+            .filter_map(|(_, v)| if !v.is_removed() { Some(v) } else { None })
+            .map(|v| mapper(&v))
             .collect()
     })
 }
